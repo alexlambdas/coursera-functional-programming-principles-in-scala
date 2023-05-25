8cfb3675919c826a5896eda52a188acd2b9f6989
EC2806B@comcel.com.co <EC2806B@comcel.com.co>
2023-05-24 22:54:47 -0500

    feat: week1 - square roots example

--- src/main/scala/week1/Module.scala
+++ src/main/scala/week1/Module.scala
@@ -6,4 +6,17 @@ object Module {
 
   def sumOfSquares(x: Double, y: Double): Double = square(x) + square(y)
 
+  def abs(x: Double): Double = if(x < 0) then -x else x
+
+  def isGoodEnough(guess: Double, x: Double): Boolean = 
+    abs(guess * guess - x) < 0.001
+
+  def improve(guess: Double, x: Double): Double = (guess + x / guess) / 2
+
+  def sqrIter(guess: Double, x: Double): Double =
+    if (isGoodEnough(guess, x)) then guess
+    else sqrIter(improve(guess, x), x)
+
+  def sqrt(x: Double): Double = sqrIter(1.0, x)
+
 }
